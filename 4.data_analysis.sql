92d7c4f2dfbebeee394e8769cf8694f34f2988f1
kaushal0077 <124752029+kaushal0077@users.noreply.github.com>
2024-01-19 00:30:08 +0530

    Update 4.data_analysis.sql

--- 4.data_analysis.sql
+++ 4.data_analysis.sql
@@ -1,1 +1,10 @@
+----------------------------------------------
+SELECT count(ride_id) as no_of_ride
+FROM `adept-lodge-374014.bikeshare_cyclistic.cleaned_combined_data` 
+group by rideable_type
 
+---------------------------------------------------
+SELECT count(ride_id)as no_of_ride,member_casual,day_of_week,start_station_name,end_station_name,month_of_start,started_at
+FROM `adept-lodge-374014.bikeshare_cyclistic.cleaned_combined_data`
+group by ride_id ,member_casual,day_of_week,start_station_name,end_station_name,month_of_start,started_at
+-----------------
