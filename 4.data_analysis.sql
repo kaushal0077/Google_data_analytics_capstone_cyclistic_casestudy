b5d71ce25cc11d1f9e4397be2f5d8159992d63ea
kaushal0077 <124752029+kaushal0077@users.noreply.github.com>
2024-01-24 22:06:05 +0530

    Update 4.data_analysis.sql

--- 4.data_analysis.sql
+++ 4.data_analysis.sql
@@ -4,9 +4,9 @@ FROM `adept-lodge-374014.bikeshare_cyclistic.cleaned_combined_data`
 group by member casual
 ---------------code for abstarcting casual-member-monthly and hourly behaviour--------------------------------
   
-SELECT count(ride_id)as no_of_ride,member_casual,day_of_week,start_station_name,end_station_name,month_of_start,started_at
+SELECT count(ride_id)as no_of_ride,member_casual,day_of_week,month_of_start_started_at
 FROM `adept-lodge-374014.bikeshare_cyclistic.cleaned_combined_data`
-group by ride_id ,member_casual,day_of_week,start_station_name,end_station_name,month_of_start,started
+group by ride_id ,member_casual,day_of_week,month_of_start,started_at
 ---------------code for abstarting the places casual member prefer to visit and from where they start--------------------------------
 select Count(ride_id) as no_of_ride,start_lat,start_lng,member_casual,start_station_name,end_lat,end_lng,end_station_name
 from `adept-lodge-374014.bikeshare_cyclistic.cleaned_combined_data`
